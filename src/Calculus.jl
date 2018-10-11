4d5293bda195614e30ea972a2d8cc8b02b6ae21a
Jorge Pérez <jperez@icf.unam.mx>
2018-10-11 12:59:32 -0500

    Remove comments

--- src/Calculus.jl
+++ src/Calculus.jl
@@ -212,11 +212,11 @@ end
 function ∂(i)
     function (f)
         arg -> begin
-            if typeof(arg[i]) <: Union{Symbolic, Differential} #length(arg[i]) == 1
+            if typeof(arg[i]) <: Union{Symbolic, Differential}
                 ϵ = makeDiff()
                 val = [i==j ? arg[j]+ϵ : arg[j] for j in eachindex(arg)]
                 extractDiff(f(UpTuple(val)), ϵ)
-            elseif typeof(arg[i]) == UpTuple # length(arg[i]) > 1
+            elseif typeof(arg[i]) == UpTuple
                 ϵv = [Symbolics.makeDiff() for i in eachindex(arg[i])]
                 UpTuple([  begin val = [i==j ? begin [l==k ? arg[j][k]+ϵv[k] : arg[j][l] for l in eachindex(arg[j])] end : arg[j] for j in eachindex(arg)]; extractDiff( f(UpTuple(val)) , ϵv[k] ) end for k in eachindex(arg[i])  ])
             end
